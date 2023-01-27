e1012b41f5aa4ab02ca772ec618cc4533f4033b6
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2023-01-27 21:42:40 +0100

    design the hero section of the home page on the mobile

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,12 +5,53 @@ import Slider from "../../components/Slider";
 // import GoogleMap from "../../components/GoogleMap";
 import Footer from "../../components/Footer";
 import Button from "../../components/Button";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PlaceIcon from "@mui/icons-material/Place";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 const index = () => {
   return (
     <div className="home-page">
       <section className="hero-section" id="/">
         <Slider />
+        <div className="welcome-title-mobile-container">
+          <h1 className="welcome-title-mobile">
+            Willkommen in unserer kieferorthopädischen Facharztpraxis in
+            Berlin-Charlottenburg.
+          </h1>
+        </div>
+        <div className={"social-container"}>
+          <div className="social-wrapper">
+            <div className="icons-container">
+              <a href="mailto:info@zahnspange-charlottenburg.de">
+                <EmailIcon className="icon" />
+              </a>
+            </div>
+            <div className="icons-container">
+              <a
+                href="https://goo.gl/maps/DHKyNRQ9tzcoiURw8"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <PlaceIcon className="icon" />
+              </a>
+            </div>
+            <div className="icons-container">
+              <a
+                data-testid="linkElement"
+                href="https://www.doctolib.de/praxis/berlin/kieferorthopaedie-dr-nawras-darkazanly/booking/new-patient?profile_skipped=true&amp;specialityId=1325"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <CalendarMonthIcon className="icon" />
+              </a>
+            </div>
+            <div className="icons-container">
+              <PhoneIcon className="icon" />
+            </div>
+          </div>
+        </div>
       </section>
       <section className="hero-section2">
         <div className="welcome-title-container">
