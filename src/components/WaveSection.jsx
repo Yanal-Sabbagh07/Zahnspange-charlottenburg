7462877287ed654979f4bd33e3874872ed13abd9
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-03-14 13:43:32 +0100

    add appointment button to the wave section and edit the fs og wave content

--- src/components/WaveSection.jsx
+++ src/components/WaveSection.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Wave from "../components/Wave";
 import "../styles/components/WaveSection.scss";
+import Button from "./Button";
 const WaveSection = (props) => {
   return (
     <>
       <Wave header={props.header} />
       <div className="WaveSection-content-container">
         <p className="WaveSection-content">{props.content}</p>
+        <Button />
       </div>
     </>
   );
