f0f7705b6c88d1aaf34a8b66256447c4e68c4f6c
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-03-15 14:01:22 +0100

    add an infoWindow on the marker and add it on the picture before loading

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import "../styles/pages/Home.scss";
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  useLoadScript,
+  InfoWindow,
+} from "@react-google-maps/api";
 import StarIcon from "@mui/icons-material/Star";
 import StarHalfIcon from "@mui/icons-material/StarHalf";
 import AltRouteIcon from "@mui/icons-material/AltRoute";
+
 const Hole = () => {
   const [userPermession, setUserPermession] = useState(false);
   const { isLoaded } = useLoadScript({
@@ -57,14 +63,15 @@ const Hole = () => {
 };
 
 function Map() {
+  const center = useMemo(() => ({ lat: 52.51712, lng: 13.30591 }), []);
   const [markerOpen, setMarkerOpen] = useState(false);
   const onMarkerClick = () => {
     setMarkerOpen(!markerOpen);
   };
   return (
     <GoogleMap
-      zoom={11.55}
-      center={{ lat: 52.50979, lng: 13.31442 }}
+      zoom={11.9}
+      center={center}
       mapContainerClassName="google-map"
       options={{
         mapTypeControl: false,
@@ -73,48 +80,98 @@ function Map() {
       }}
     >
       <Marker
-        position={{ lat: 52.51712, lng: 13.30591 }}
+        position={center}
         onClick={onMarkerClick}
         // label={{
         //   text: "zahnspange charlottenburg",
         //   fontSize: "14px",
         //   labelColor: "red",
         // }}
-      />
-      {markerOpen && (
-        <div className="gmap-div-container">
-          <div className="gmap-content">
-            <div className="gmap-name">
-              Zahnspange Charlottenburg / <br />
-              Dr.Nawras Darkazanli
-            </div>
-            <div className="name-txt">
-              Richard-Wagner-Platz 3 <br /> 10585 Berlin
-            </div>
-            <div className="bewertung">
-              <span>4,3</span>
-              <StarIcon className="full-star" />
-              <StarIcon className="full-star" />
-              <StarIcon className="full-star" />
-              <StarIcon className="full-star" />
-              <StarHalfIcon className="half-star" />
-              <span>(45)</span>
-            </div>
-          </div>
-          <div className="gmap-direction">
-            <div className="route-icon-container">
-              <AltRouteIcon className="route-icon" />
-            </div>
-            <a
-              href="https://goo.gl/maps/DHKyNRQ9tzcoiURw8"
-              target="_blank"
-              rel="noreferrer noopener"
+      >
+        {markerOpen && (
+          <InfoWindow
+            position={{ lat: 52.51712, lng: 13.30591 }}
+            options={{ minWidth: 300 }}
+            onCloseClick={() => {
+              setMarkerOpen(false);
+            }}
+          >
+            <div
+              className="gmap-div-container"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                color: "black",
+                height: "116px",
+              }}
             >
-              Routenplaner
-            </a>
-          </div>
-        </div>
-      )}
+              <div
+                className="gmap-content"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  textAlign: "left",
+                  height: "90%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div
+                  className="gmap-name"
+                  style={{
+                    color: "black",
+                    fontWeight: "600",
+                  }}
+                >
+                  Zahnspange Charlottenburg / <br />
+                  Dr.Nawras Darkazanli
+                </div>
+                <div className="name-txt">
+                  Richard-Wagner-Platz 3 <br /> 10585 Berlin
+                </div>
+                <div
+                  className="bewertung"
+                  style={{ color: "gold", display: "flex" }}
+                >
+                  <span style={{ color: "gray" }}>4,3</span>
+                  <StarIcon className="full-star" />
+                  <StarIcon className="full-star" />
+                  <StarIcon className="full-star" />
+                  <StarIcon className="full-star" />
+                  <StarHalfIcon className="half-star" />
+                  <span style={{ color: "gray" }}>(45)</span>
+                </div>
+              </div>
+              <div className="gmap-direction">
+                <div className="route-icon-container">
+                  <AltRouteIcon
+                    className="route-icon"
+                    style={{
+                      fontSize: "38px",
+
+                      color: "#20ade4",
+                    }}
+                  />
+                </div>
+                <a
+                  href="https://goo.gl/maps/DHKyNRQ9tzcoiURw8"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  style={{
+                    textDecoration: "none",
+                    color: "#1f5ca2",
+                    fontSize: "14px",
+                    fontWeight: "400",
+                  }}
+                >
+                  Routenplaner
+                </a>
+              </div>
+            </div>
+          </InfoWindow>
+        )}
+      </Marker>
     </GoogleMap>
   );
 }
